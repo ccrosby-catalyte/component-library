5d15260be35156d3628494457a6b5d0b04c4f28e
Chris Crosby <ccrosby@catalystdevworks.com>
2021-01-21 23:35:54 -0500

    Init Commit

---
+++ component-library/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import Input from './components/input.js';
+import './App.css';
+
+function App() {
+  return (
+    <Input></Input>
+  );
+}
+
+export default App;
---
+++ component-library/src/components/input.js
@@ -0,0 +1,13 @@
+
+import '../App.css';
+
+function Input() {
+  return (
+    <div>
+      <label htmlFor="input">Input one</label>
+      <input id="input" type="text" />
+    </div>
+  );
+}
+
+export default Input;
